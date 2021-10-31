34a3ae58dfcac57d10f76b690fe3ce590df5cce0
SaurabhMallik <44564406+SaurabhMallik@users.noreply.github.com>
2021-10-31 18:03:34 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(false), cin.tie(0);
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+const int N = 1e9;
+
+using namespace std;
+
+void solve()
+{
+  cout<<"Test"<<endl;
+}
+
+int main()
+{
+  fast;
+  int t;
+  cin>>t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
